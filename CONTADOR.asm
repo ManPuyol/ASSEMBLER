ca70890966c846a1c3cf23c2a2f9c8bb00ab96c1
ManPuyol <72559693+ManPuyol@users.noreply.github.com>
2021-01-11 09:51:13 +0100

    Optimization

--- CONTADOR.asm
+++ CONTADOR.asm
@@ -1,18 +1,16 @@
 ;http://schweigi.github.io/assembler-simulator/index.html
 
 	JMP start
-min: DB 1 ; Variable
+min: DB 0 ; Variable
 max: DB 5 ; Variable
 
 start:
 	
 	MOV D, 232	; Point to output
-	CALL print
-        HLT             ; Stop execution
+	CALL prepare
 
-print:	MOV C, [min]	; Point to var 
-	MOV B, [max]
-	ADD B, 1
+prepare: MOV B, [max]
+	 ADD B, 1
 .loop:
 	MOV A, C	; 
 	ADD A, 48
@@ -20,6 +18,5 @@ print:	MOV C, [min]	; Point to var
 	INC C  
 	CMP B, C	; Check if end
 	JNZ .loop	; jump if not
-	JMP print ; reiniciar a 1
-
-	RET
+	MOV C, [min]	; Point to var 
+	JMP .loop
